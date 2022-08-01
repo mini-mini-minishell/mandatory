aa8d4a1fa6e4ccb1101c0cc2056deebd7fa8bf70
eunsiho <versailles@korea.ac.kr>
2022-08-01 09:44:02 +0900

    init

---
+++ main.c
@@ -0,0 +1,9 @@
+int	main(int argc, char **argv, char **envp)
+{
+	// 파싱부 (파싱 테이블 + 트리 구현)
+		// lexer : 토큰 나누는 곳
+		// parser : 트리에 저장하고 트리를 만듦, 테이블 구현
+	// 프롬프트 띄우기
+	// 실행부(bulit-in, executable)
+	return (0);
+}
