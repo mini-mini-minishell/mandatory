c7362f3b8ab48a98586cca350a0494ed1b4037a7
Haeun Jeong <gkdms4248@gmail.com>
2022-08-02 15:29:10 +0900

    jjunhyuki

--- sources/list_envp.c
+++ sources/list_envp.c
@@ -3,4 +3,5 @@
 t_node	*envp_search_node(t_list *list, char *key)
 {
     /* key값을 비교하기위해 strlen, strncmp
-     를 가져와서 비교를 핧 예정이다... 언제?? 내일!!!(수요일) */ }
+     를 가져와서 비교를 핧 예정이다... 언제?? 내일!!!(수요일) */ 
+}
