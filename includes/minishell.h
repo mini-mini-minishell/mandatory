878866c35e1aed5b3ef520761c24b4334db0075b
ssolpang <iless3377@naver.com>
2022-08-02 14:24:36 +0900

    jkwak

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,6 +1,25 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+typedef struct s_node
+{
+	void	*data;
+	t_node	*next;
+}	t_node;
+
+typedef struct s_list
+{
+	t_node	*head;
+	t_node	*tail;
+	size_t	count;
+}	t_list;
+
+typedef struct s_env_data
+{
+	char	*key;
+	char	*value;
+}	t_env_data;
+
 /* parsing.c */
 void	check_arguments(int	argc, char **argv);
 
