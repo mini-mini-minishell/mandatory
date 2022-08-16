9b8ab20f8020e6787821c317ca785cb0453ce924
jjunhyuki <yjs99004@naver.com>
2022-08-16 09:46:41 +0900

    jjunhyuki

--- sources/reduce_func1.c
+++ sources/reduce_func1.c
@@ -2,12 +2,24 @@
 
 t_return_value	reduce_rule_0(t_all_data *all_data)
 {
-	int	count;
+	int			count;
+	t_parser	*parser;
 
+	parser = &all_data->parser;
 	count = 2 * 1;
-	pop_parser_stack(parser, count);
+	stack_pop_back(&parser->parser_stack, count);
+	list_push_back(&parser->parser_stack, list_new_node(create_parser_data()));
 	push_parser_stack(parser, SYMBOL_START);
 	parser->flag |= PARSER_FINISH;
 	parser->final_cmd = parser->value_stack->content.cmd;
 	return (RV_SUCCESS);
 }
+
+t_return_value	reduce_rule_1(t_all_data *all_data)
+{
+
+}
+
+//list_push_back(&parser->parser_stack, list_new_node(create_parser_data()));
+
+
