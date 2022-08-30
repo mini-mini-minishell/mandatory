32556b00382f1e35fc0e0416fac5c0076f8f1792
김재환 <kimjaehwan@gimjaehwan-ui-MacBookAir.local>
2022-08-30 09:29:25 +0900

    jaehwkim

--- sources/ft_exit.c
+++ sources/ft_exit.c
@@ -9,6 +9,33 @@ static int	ft_isdigit(int c)
 	return (c >= 48 && c <= 57);
 }
 
+int	ft_atoi(const char *str)
+{
+	unsigned long long	result;
+	int					plma;
+	int					index;
+
+	index = 0;
+	while (str[index] == ' ' || (str[index] >= 9 && str[index] <= 13))
+		index++;
+	plma = 1;
+	if (str[index] == '-')
+		plma = -1;
+	if (str[index] == '-' || str[index] == '+')
+		index++;
+	result = 0;
+	while (str[index] >= '0' && str[index] <= '9')
+	{
+		result = result * 10 + str[index] - '0';
+		index++;
+	}
+	if (result > 9223372036854775807 && plma == 1)
+		return (-1);
+	else if (result > 9223372036854775808ULL && plma == -1)
+		return (0);
+	return ((int)result * plma);
+}
+
 static int	are_digit(char *target)
 {
 	size_t	i;
